61085053a89fa9cedbcf711e71798e17bc978f80
Tarmigan Casebolt <tarmigan+git@gmail.com>
2011-03-07 22:34:10 -0800

    Windows serial code that works (very slowly)

--- serial_windows.go
+++ serial_windows.go
@@ -2,6 +2,10 @@ package serial
 
 // #include <windows.h>
 import "C"
+import (
+	"os"
+	"unsafe"
+)
 
 func OpenPort(name string, baud int) (*os.File, os.Error) {
 	f, err := os.Open(name, os.O_RDWR, 0666)
@@ -9,22 +13,26 @@ func OpenPort(name string, baud int) (*os.File, os.Error) {
 		return nil, err
 	}
 
-	handle := f.Fd()
-	var params C.DCB
-	params.DCBLength = C.sizeof(C.DCB)
-	if ok, err := C.GetCommState(handle, &params); !ok {
+	type hp *C.HANDLE
+	var handle *C.HANDLE
+	fd := f.Fd()
+	handle = hp(unsafe.Pointer(&fd))
+	var params C.struct__DCB
+	params.DCBlength = C.DWORD(unsafe.Sizeof(params))
+	if ok, err := C.GetCommState(*handle, &params); ok == C.FALSE {
+		f.Close()
 		return nil, err
 	}
 
-	params.BaudRate = baud
+	params.BaudRate = C.DWORD(baud)
 	params.ByteSize = 8
 	params.StopBits = C.ONESTOPBIT
 	params.Parity   = C.NOPARITY
 
-
-	if ok, err := C.SetCommState(handle, &params); !ok {
+	if ok, err := C.SetCommState(*handle, &params); ok == C.FALSE {
+		f.Close()
 		return nil, err
 	}
-	
+
 	return f, nil
 }
